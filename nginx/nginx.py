df461fc3b8222387249e7bc26e1b09242b6fb945
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2017-08-26 19:30:11 +0200

    Use relative paths.

--- nginx/nginx.py
+++ nginx/nginx.py
@@ -1,7 +1,10 @@
+from os.path import join, split
 import sys
 
 server_name = "{}.hackerspace-ntnu.no www.{}.hackerspace-ntnu.no {}.idi.ntnu.no www.{}.idi.ntnu.no;"
 TEMPLATE_PATH = './templates/site'
+
+ROOT_PATH = split(__file__)[0]
 
 
 def generate_nginx_config(subdomain):
@@ -9,11 +12,11 @@ def generate_nginx_config(subdomain):
     Reads in a file from templates and inserts the correct server_name according to the subdomain argument.
     """
     server_name_str = server_name.format(*[subdomain] * 4)
-    with open(TEMPLATE_PATH) as file:
+    with open(join(ROOT_PATH, TEMPLATE_PATH)) as file:
         config = file.read()
         final_config = config % {'server_name': server_name_str}
 
-        with open('./sites-enabled/site', 'w') as config_file:
+        with open(join(ROOT_PATH, 'sites-enabled/site'), 'w') as config_file:
             config_file.write(final_config)
 
 
