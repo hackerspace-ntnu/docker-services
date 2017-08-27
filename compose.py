383906bda2e78eb2ac3d33b68c565762a6d6dccf
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2017-08-27 15:08:02 +0200

    Switch ports.

--- compose.py
+++ compose.py
@@ -4,7 +4,7 @@ import sys
 def generate_docker_compose(subdomain, port):
     with open('docker-compose.yml', 'r') as f:
         content = f.read()
-        compose = content % {'subdomain': subdomain, 'website_port': port, 'proxy_port': int(port) + 1}
+        compose = content % {'subdomain': subdomain, 'website_port': int(port) + 1, 'proxy_port': port}
 
     with open('docker-compose.yml', 'w') as f:
         f.write(compose)
