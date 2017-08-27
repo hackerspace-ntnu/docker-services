cdbf7eede6986745a8e67b2ef446402624220b96
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2017-08-27 13:32:57 +0200

    Edit script for generating outer nginx conf.

--- nginx/nginx.py
+++ nginx/nginx.py
@@ -2,23 +2,25 @@ from os.path import join, split
 import sys
 
 server_name = "{}.hackerspace-ntnu.no www.{}.hackerspace-ntnu.no {}.idi.ntnu.no www.{}.idi.ntnu.no;"
-TEMPLATE_PATH = './templates/site'
+
+TEMPLATE_PATH = './templates/outer_site_config'
+DESTINATION_PATH = '/etc/nginx/sites-enabled/%(subdomain)s'
 
 ROOT_PATH = split(__file__)[0]
 
 
-def generate_nginx_config(subdomain):
+def generate_nginx_config(subdomain, port):
     """
     Reads in a file from templates and inserts the correct server_name according to the subdomain argument.
     """
     server_name_str = server_name.format(*[subdomain] * 4)
     with open(join(ROOT_PATH, TEMPLATE_PATH)) as file:
         config = file.read()
-        final_config = config % {'server_name': server_name_str}
+        final_config = config % {'server_name': server_name_str, 'port': port}
 
-        with open(join(ROOT_PATH, 'sites-enabled/site'), 'w') as config_file:
+        with open(DESTINATION_PATH % {'subdomain': subdomain}, 'w') as config_file:
             config_file.write(final_config)
 
 
 if __name__ == '__main__':
-    generate_nginx_config(sys.argv[1])
+    generate_nginx_config(*sys.argv[1:3])
