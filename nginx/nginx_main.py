69a93b2798c226fbe2828ef58aab94737ae19af6
Amund Tenstad <github@amund.io>
2017-08-27 19:11:11 +0200

    Fix server_name argument

--- nginx/nginx_main.py
+++ nginx/nginx_main.py
@@ -6,7 +6,7 @@ DESTINATION_PATH = '/etc/nginx/sites-enabled/main.conf'
 def generate_main_nginx_config(server_name, domain):
     with open(TEMPLATE_PATH, 'r') as f:
         content = f.read()
-        config = content % {'server_name': server_name, 'domain': domain}
+        config = content % {'server_name': server_name.replace('__', ' '), 'domain': domain}
         with open(DESTINATION_PATH, 'w') as f:
             f.write(config)
 
